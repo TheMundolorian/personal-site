793e86fbc175156f19613a08aa8447dfce66728c
TheMundolorian <100098652+TheMundolorian@users.noreply.github.com>
2026-03-20 09:11:52 -0400

    Fixes

--- src/data/resume/courses.ts
+++ src/data/resume/courses.ts
@@ -10,16 +10,19 @@ const courses: Course[] = [
     title: 'Microsoft Certified: Azure AI Fundamentals',
     number: '926D4B827D5AE4AF',
     link: 'https://www.linkedin.com/redir/redirect/?url=https%3A%2F%2Flearn.microsoft.com%2Fapi%2Fcredentials%2Fshare%2Fen-us%2FMarcosRaymundo-1027%2F926D4B827D5AE4AF%3FsharingId&urlhash=fGW3&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B0jr9HOdkSeesed1Cpjv%2BbA%3D%3D',
+    university: 'Microsoft',
   },
   {
     title: 'Microsoft Applied Skills: Create an AI agent',
     number: '64215B076983E8D0',
     link: 'https://www.linkedin.com/redir/redirect/?url=https%3A%2F%2Flearn.microsoft.com%2Fapi%2Fcredentials%2Fshare%2Fen-us%2FMarcosRaymundo-1027%2F64215B076983E8D0%3FsharingId&urlhash=4VR1&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3B0jr9HOdkSeesed1Cpjv%2BbA%3D%3D',
+    university: 'Microsoft',
   },
   {
     title: 'Copilot Champs | Training Achievement',
     number: '64215B076983E8D0',
     link: 'https://www.linkedin.com/redir/redirect/?url=https%3A%2F%2Fwww.credly.com%2Fbadges%2Fb20c8da2-0d66-48b4-9463-05229820340b%2Flinked_in_profile&urlhash=KHE-&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BEa%2FAQH%2FrRM%2BQtNSMXPSroQ%3D%3D',
+    university: 'Microsoft',
   },
 ];
 
