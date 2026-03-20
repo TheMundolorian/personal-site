76480b04d524e330afadba8d52ec65d304c2a74e
TheMundolorian <100098652+TheMundolorian@users.noreply.github.com>
2026-03-20 09:19:57 -0400

    Updates to resume nav and courses

--- src/data/resume/courses.ts
+++ src/data/resume/courses.ts
@@ -8,20 +8,26 @@ export interface Course {
 const courses: Course[] = [
   {
     title: 'Microsoft Certified: Azure AI Fundamentals',
-    number: '926D4B827D5AE4AF',
-    link: 'https://www.linkedin.com/redir/redirect/?url=https%3A%2F%2Flearn.microsoft.com%2Fapi%2Fcredentials%2Fshare%2Fen-us%2FMarcosRaymundo-1027%2F926D4B827D5AE4AF%3FsharingId&urlhash=fGW3&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B0jr9HOdkSeesed1Cpjv%2BbA%3D%3D',
+    number: 'Credential ID: 926D4B827D5AE4AF',
+    link: 'https://learn.microsoft.com/en-us/users/marcosraymundo-1027/credentials/926d4b827d5ae4af',
     university: 'Microsoft',
   },
   {
     title: 'Microsoft Applied Skills: Create an AI agent',
-    number: '64215B076983E8D0',
-    link: 'https://www.linkedin.com/redir/redirect/?url=https%3A%2F%2Flearn.microsoft.com%2Fapi%2Fcredentials%2Fshare%2Fen-us%2FMarcosRaymundo-1027%2F64215B076983E8D0%3FsharingId&urlhash=4VR1&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3B0jr9HOdkSeesed1Cpjv%2BbA%3D%3D',
+    number: 'N/A',
+    link: 'https://learn.microsoft.com/en-us/users/marcosraymundo-1027/credentials/64215b076983e8d0',
     university: 'Microsoft',
   },
   {
     title: 'Copilot Champs | Training Achievement',
-    number: '64215B076983E8D0',
-    link: 'https://www.linkedin.com/redir/redirect/?url=https%3A%2F%2Fwww.credly.com%2Fbadges%2Fb20c8da2-0d66-48b4-9463-05229820340b%2Flinked_in_profile&urlhash=KHE-&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BEa%2FAQH%2FrRM%2BQtNSMXPSroQ%3D%3D',
+    number: 'Credential ID: 64215B076983E8D0',
+    link: 'https://www.credly.com/badges/b20c8da2-0d66-48b4-9463-05229820340b/linked_in_profile',
+    university: 'Microsoft',
+  },
+  {
+    title: 'Microsoft Applied Skills: Create and manage canvas apps with Power Apps',
+    number: 'N/A',
+    link: 'http://learn.microsoft.com/en-us/users/marcosraymundo-1027/credentials/ea3c27a63bbb5fba',
     university: 'Microsoft',
   },
 ];
